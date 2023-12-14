155f0d4588ebb543baf1177d5db1fe061a9882ee
adzict <adzic.tanja@gmail.com>
2023-12-14 14:13:33 +0100

    Update assignment.sql

--- assignment.sql
+++ assignment.sql
@@ -2,3 +2,14 @@
 
 -- staff, sales_outlet, sales_transaction, customer, product
 
+-- List of attributes for the sales_transaction entity
+
+-- product_id, customer_id, staff_id, transaction_id, sales_outlet_id, price, quantity, transaction_time, transaction_date
+
+-- List of attributes for other tables:
+
+-- staff: staff_id, first_name, last_name, position, start_date, location
+-- sales_outlet: sales_outlet_id, sales_outlet_type, address, city, telephone, postal_code, manager
+-- customer: customer_id, customer_name, customer_email, customer_since, customer_card_number, birthdate, gender
+-- product: product_id, product_category, product_type, product_name, description, price
+
